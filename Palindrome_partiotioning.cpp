23840bbaf8e39cb0da8a70422e17e66e8cedb454
Akash-Shrivastava11 <72738431+Akash-Shrivastava11@users.noreply.github.com>
2021-07-20 10:52:57 +0530

    Memoized  mcm based solution.
    to find minimum number of partition in the given string , s.t. the partitions are palindrome.

--- Palindrome_partiotioning.cpp
+++ Palindrome_partiotioning.cpp
@@ -1,6 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
-
+static int dp[100][100];
 bool isPal(string x, int i,int j)
 {
     // cout<<i<<j;
@@ -20,17 +20,19 @@ int palpart(string name,int i,int j)
 {
     if(i>=j) return 0;
     if(isPal(name,i,j)) return 0;
+    if(dp[i][j]!=-1)return dp[i][j];
     int mn=INT_MAX;
     for(int k=i;k<=j-1;k++)
     {
         int temp=palpart(name,i,k)+palpart(name,k+1,j)+1;
         mn=min(mn,temp);
     }
-    return mn;
+    return dp[i][j]=mn;
 }
 
 int main()
 {
+    memset(dp,-1,sizeof(dp));
     string name;
     cout<<"Enter the string";
     cin>>name;
